0e31a195088ab7a52834d3307ec78fcd521f5cf2
Legion154 <darkdestiny700@gmail.com>
2024-11-27 23:39:55 +0500

    bug p1

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import bg from "../../assets/aboutBg.png";
 import mobility from "../../assets/mobilityAbout.png";
-import urban from "../../assets/urbanABout.png";
+import urbanAbout from "../../assets/urbanABout.png";
 import tech from "../../assets/tech.png";
 import integrity from "../../assets/integrity.png";
 import commmunity from "../../assets/community.png";
@@ -67,7 +67,7 @@ const About = () => {
           </div>
           <span className="flex flex-row items-center relative">
             <img
-              src={urban}
+              src={urbanAbout}
               alt="example picture"
               className="rounded-full relative w-60 2xl:w-[700px]"
             />
